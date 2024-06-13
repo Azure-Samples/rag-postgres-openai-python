4319316e696e7bd7b0ec59daa8078f8001002056
Pamela Fox <pamela.fox@gmail.com>
2024-06-12 21:45:00 -0700

    Improve the bash scripts

---
+++ scripts/load_python_env.sh
@@ -0,0 +1,8 @@
+ #!/bin/sh
+
+echo 'Creating Python virtual environment in .venv...'
+python3 -m venv .venv
+
+echo 'Installing dependencies from "requirements.txt" into virtual environment (in quiet mode)...'
+.venv/bin/python -m pip --quiet --disable-pip-version-check install -e src
+.venv/bin/python -m pip --quiet --disable-pip-version-check install -r requirements-dev.txt
--- scripts/setup_postgres_seeddata.sh
+++ scripts/setup_postgres_seeddata.sh
@@ -2,4 +2,6 @@ POSTGRES_HOST=$(azd env get-values | grep POSTGRES_HOST | sed 's/="/=/' | sed 's
 POSTGRES_USERNAME=$(azd env get-values | grep POSTGRES_USERNAME | sed 's/="/=/' | sed 's/"$//' | sed 's/^POSTGRES_USERNAME=//')
 POSTGRES_DATABASE=$(azd env get-values | grep POSTGRES_DATABASE | sed 's/="/=/' | sed 's/"$//' | sed 's/^POSTGRES_DATABASE=//')
 
-python ./src/fastapi_app/setup_postgres_seeddata.py --host $POSTGRES_HOST --username $POSTGRES_USERNAME --database $POSTGRES_DATABASE
+. ./scripts/load_python_env.sh
+
+.venv/bin/python ./src/fastapi_app/setup_postgres_seeddata.py --host $POSTGRES_HOST --username $POSTGRES_USERNAME --database $POSTGRES_DATABASE
