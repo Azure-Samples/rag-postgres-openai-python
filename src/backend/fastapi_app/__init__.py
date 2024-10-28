5892cb73dbe97f3c37356a38408063c6cea670bb
Pamela Fox <pamela.fox@gmail.com>
2024-10-28 20:53:25 +0000

    Use westus endpoint for chat completions

--- src/backend/fastapi_app/__init__.py
+++ src/backend/fastapi_app/__init__.py
@@ -52,7 +52,7 @@ def create_app(testing: bool = False):
     else:
         if not testing:
             load_dotenv(override=True)
-        logging.basicConfig(level=logging.INFO)
+        logging.basicConfig(level=logging.DEBUG)
     # Turn off particularly noisy INFO level logs from Azure Core SDK:
     logging.getLogger("azure.core.pipeline.policies.http_logging_policy").setLevel(logging.WARNING)
     logging.getLogger("azure.identity").setLevel(logging.WARNING)
--- src/backend/fastapi_app/openai_clients.py
+++ src/backend/fastapi_app/openai_clients.py
@@ -14,7 +14,7 @@ async def create_openai_chat_client(
     OPENAI_CHAT_HOST = os.getenv("OPENAI_CHAT_HOST")
     if OPENAI_CHAT_HOST == "azure":
         api_version = os.environ["AZURE_OPENAI_VERSION"] or "2024-03-01-preview"
-        azure_endpoint = os.environ["AZURE_OPENAI_ENDPOINT"]
+        azure_endpoint = "https://pf-openai-keylesslf653mu3fc22i-openai.openai.azure.com/"
         azure_deployment = os.environ["AZURE_OPENAI_CHAT_DEPLOYMENT"]
         if api_key := os.getenv("AZURE_OPENAI_KEY"):
             logger.info(
